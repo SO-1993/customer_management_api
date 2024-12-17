bb3a5fe3afa60de485f00e7b0cfb15c9537ff5fc
Scott <scottobrien93@outlook.com>
2024-12-17 12:58:07 +0000

    Implemented POST /customers to add customer

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // CRUD Endpoints:
 // Implement the following API endpoints:
 
-//     POST /customers: Add a new customer with fields like id, name, email, and phone.
 //     GET /customers: Retrieve a list of all customers.
 //     GET /customers/:id: Retrieve a specific customer by ID.
 //     PUT /customers/:id: Update an existing customer's details.
@@ -18,9 +17,36 @@ app.listen(PORT, () => {
   console.log("Server Listening on PORT:", PORT);
 });
 
+// GET /: Check that connection exists on homepage
+app.get("/", (req, res) => {
+  const welcomeMessage = {
+    message: "Welcome to the Customer API",
+  };
+  res.status(200).send(welcomeMessage);
+});
+
+// GET /status: Check that status is running
 app.get("/status", (req, res) => {
   const status = {
-    Status: "Running",
+    status: "Running",
   };
-  res.send(status);
+  res.status(200).send(status);
+});
+
+// POST /customers: Add a new customer with fields like id, name, email, and phone.
+app.post("/customers", (req, res) => {
+  const { id, name, email, phone } = req.body;
+
+  if (!id || !name || !email || !phone) {
+    return res.status(400).send({
+      error:
+        "Missing required fields. Please provide id, name, email and phone",
+    });
+  }
+  const newCustomer = { id, name, email, phone };
+
+  res.status(201).send({
+    message: "Customer created successfully",
+    customer: newCustomer,
+  });
 });
